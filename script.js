8a1387fb43cc2191492dbf8493737525b2c89125
snake324 <kristi4n90@gmail.com>
2023-06-21 09:17:16 +0200

    html

---
+++ script.js
@@ -0,0 +1,29 @@
+async function obtenerDatos() {
+    const respuesta = await fetch('https://jsonplaceholder.typicode.com/users');
+    const datos = await respuesta.json();
+    return datos;
+}
+
+async function imprimirDatos() {
+    const datos = await obtenerDatos();
+    console.log(datos);
+}
+imprimirDatos();
+
+async function crearTabla() {
+    const datos = await obtenerDatos();
+    let tabla = '<table><tr><th>ID</th><th>Nombre</th><th>Ciudad</th></tr>';
+    datos.forEach(usuario => {
+        tabla += `<tr><td>${usuario.id}</td><td>${usuario.name}</td><td>${usuario.address.city}</td></tr>`;
+    });
+    tabla += '</table>';
+    document.body.innerHTML = tabla;
+}
+crearTabla();
+
+async function obtenerUsuario() {
+    const id = document.getElementById('idUsuario').value;
+    const respuesta = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+    const usuario = await respuesta.json();
+    document.getElementById('resultado').innerHTML = `Nombre: ${usuario.name}, Teléfono: ${usuario.phone}`;
+}

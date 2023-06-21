ab4889a7fea7540c828d957b7075ac194ee47950
snake324 <kristi4n90@gmail.com>
2023-06-21 09:18:42 +0200

    print

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ async function obtenerDatos() {
     const datos = await respuesta.json();
     return datos;
 }
+
+
+async function imprimirDatos() {
+    const datos = await obtenerDatos();
+    console.log(datos);
+}
+imprimirDatos();
+
